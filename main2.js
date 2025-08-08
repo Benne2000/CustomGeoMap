b843404d2a80a59f12a37b7dd20be03a0e17b06e
Benne2000 <benedikt.stockert00@gmail.com>
2025-08-08 14:53:02 +0200

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,30 +1,11 @@
 (function () {
-  let leafletLoaded = false;
-
-  function loadLeaflet() {
-    if (leafletLoaded) return Promise.resolve();
-    leafletLoaded = true;
-
-    return new Promise((resolve, reject) => {
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
-      document.head.appendChild(link);
-
-      const script = document.createElement("script");
-      script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
-      script.onload = resolve;
-      script.onerror = reject;
-      document.head.appendChild(script);
-    });
-  }
-
   class LeafletMap extends HTMLElement {
     constructor() {
       super();
       this._map = null;
       this._markers = [];
       this._resizeObserver = null;
+      this._leafletReady = false;
     }
 
     connectedCallback() {
@@ -33,39 +14,49 @@
 
       this.innerHTML = `<div id="map" style="width:100%;height:100%;position:absolute;top:0;left:0;"></div>`;
 
-      const waitForVisibleSize = () => {
-        const rect = this.getBoundingClientRect();
-        return rect.width > 0 && rect.height > 0;
-      };
-
-      const tryInit = () => {
-        if (waitForVisibleSize()) {
-          loadLeaflet().then(() => {
-            this._initMap();
-
-            // Verzögertes invalidateSize nach Initialisierung
-            setTimeout(() => {
-              if (this._map) this._map.invalidateSize();
-            }, 300);
-          });
-
-          this._resizeObserver = new ResizeObserver(() => {
-            if (this._map) {
-              setTimeout(() => this._map.invalidateSize(), 100);
-            }
-          });
-          this._resizeObserver.observe(this);
-        } else {
-          // Wiederhole bis sichtbar
-          setTimeout(tryInit, 100);
+      this._resizeObserver = new ResizeObserver(() => {
+        if (this._map) {
+          setTimeout(() => this._map.invalidateSize(), 100);
         }
-      };
+      });
+      this._resizeObserver.observe(this);
 
-      tryInit();
+      this._waitForVisibleSize().then(() => this._loadLeaflet().then(() => this._initMap()));
     }
 
     disconnectedCallback() {
       if (this._resizeObserver) this._resizeObserver.disconnect();
+    }
+
+    _waitForVisibleSize() {
+      return new Promise(resolve => {
+        const check = () => {
+          const rect = this.getBoundingClientRect();
+          if (rect.width > 0 && rect.height > 0) {
+            resolve();
+          } else {
+            setTimeout(check, 100);
+          }
+        };
+        check();
+      });
+    }
+
+    _loadLeaflet() {
+      if (window.L) return Promise.resolve(); // schon geladen
+
+      return new Promise((resolve, reject) => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
+        document.head.appendChild(link);
+
+        const script = document.createElement("script");
+        script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+      });
     }
 
     _initMap() {
@@ -75,6 +66,9 @@
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: '&copy; OpenStreetMap contributors'
       }).addTo(this._map);
+
+      // WICHTIG: invalidateSize nach Initialisierung
+      setTimeout(() => this._map.invalidateSize(), 300);
     }
 
     onCustomWidgetResize() {
@@ -118,5 +112,6 @@
     }
   }
 
+  // WICHTIG: Widget sofort registrieren
   customElements.define("leaflet-map", LeafletMap);
 })();
